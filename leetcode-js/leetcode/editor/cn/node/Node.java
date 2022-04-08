9f16e6fb260c16e7093499b4488b6c5c4fa06222
AlvinScrp <351033166@qq.com>
2022-04-08 13:18:31 +0800

    提交冗余文件

--- leetcode-js/leetcode/editor/cn/node/Node.java
+++ leetcode-js/leetcode/editor/cn/node/Node.java
@@ -1,13 +1,22 @@
 package node;
 
+import java.util.List;
+
 public class Node {
     public int val;
     public Node next;
     public Node random;
 
+    public List<Node> children;
+
     public void nextAndRandom(Node next, Node random) {
         this.next = next;
         this.random = random;
+    }
+
+    public Node(int val, List<Node> children) {
+        this.val = val;
+        this.children = children;
     }
 
     public Node(int val) {
